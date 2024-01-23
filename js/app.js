86f934f067e21db30c08d337e0e792fbf29fb09b
Muhammad Yusuf Hamidov <muhammadyusufhamidov@Muhammads-MacBook-Air.local>
2024-01-23 19:52:14 +0500

    home work

---
+++ js/app.js
@@ -0,0 +1,94 @@
+// const userDegree = {
+//   jack: "asperantura",
+//   insporationjon: "master's",
+//   yusuf: "medium special",
+
+//   kimdur: {
+//     bilim: false,
+//     lessons: undefined,
+//     home_work: null,
+//   },
+
+//   getFullName: function () {
+//     console.log("insporationjon , age: 20; master's");
+//   },
+
+//   getThisName: function () {
+//     console.log(this.jack + " " + this.insporationjon);
+//   }
+// };
+
+// userDegree.akrom = "master's";
+
+// delete userDegree.kimdur;
+
+// console.log(userDegree);
+
+// 7-misol
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// function x(arr) {
+//   l = 6;
+//   for (let i = 1; i <= arr.length; i++) {
+//     if (i < l) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// x(arr);
+
+// let products = [
+//   {
+//     name: "Olma",
+//     price: 15000,
+//     kg: 2
+//   },
+//   {
+//     name: "Kartoshka",
+//     price: 5000,
+//     kg: 5
+//   },
+//   {
+//     name: "Non",
+//     price: 2800,
+//     kg: 3
+//   }
+// ];
+
+// let allSumma = 0;
+// function product(products) {
+//   for (let i = 0; i < products.length; i++) {
+//     allSumma += products[i].price;
+//   }
+// }
+// product(products);
+
+// console.log(allSumma);
+
+
+
+
+// const users = [
+//   {
+//     name: "Abdulaziz",
+//     age: 22,
+//   },
+//   {
+//     name: "Erkin",
+//     age: 33,
+//   },
+//   {
+//     name: "Temur",
+//     age: 46,
+//   },
+// ];
+
+// function user(people) {
+//   for (let i = 0; i < people.length; i++) {
+//     console.log(people[i].age);
+//   }
+// }
+
+// user(users);
